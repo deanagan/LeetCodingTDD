e5712ec9bfe11b766ef00dc123423c71910216d9
Dean Agan <agandfr@gmail.com>
2020-04-04 10:39:21 +1100

    Fixed bug when a carry exists and lists are exhausted Swapped checking for null so we do things in if always

--- AddTwoNumbers/Lib/AddTwoNumbers.cs
+++ AddTwoNumbers/Lib/AddTwoNumbers.cs
@@ -22,22 +22,22 @@ namespace Lib
             ListNode result = null;
             ListNode last = null;
             var carry = 0;
-            while (l1 != null || l2 != null)
+            while (l1 != null || l2 != null || carry > 0)
             {
                 var num = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
                 carry = (num >= 10) ? 1 : 0;
                 var temp = new ListNode(num % 10);
                 temp.next = null;
 
-                if (result == null)
-                {
-                    result = temp;
-                    last = result;
-                }
-                else
+                if (result != null)
                 {
                     last.next = temp;
                     last = temp;
+                }
+                else
+                {
+                    result = temp;
+                    last = result;
                 }
                 l1 = l1?.next ?? null;
                 l2 = l2?.next ?? null;
