4f1355b005dfab0fae7478ddabc9ef36c337a126
Dean Agan <agandfr@gmail.com>
2020-04-18 18:19:35 +1000

    Initial project and sln for this exercise

---
+++ PrimeFactors/PrimeFactorsLib/PrimeFactors.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeFactorsLib
+{
+    public class PrimeFactors
+    {
+        public List<int> PrimeFactorsOf(int number)
+        {
+            return new List<int>();
+        }
+    }
+}
---
+++ PrimeFactors/PrimeFactorsTest/PrimeFactorsTest.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+using FluentAssertions;
+
+namespace PrimeFactorsLib.Test
+{
+    public class PrimeFactorsShould
+    {
+        [Theory]
+        [InlineData(new object[]{ new int[]{1,2,5}, 10})]
+        public void BeCorrect_WhenGettingPrimeFactorsOfNumber(int[] expected, int number)
+        {
+            // Arrange
+            var sut = new PrimeFactors();
+
+            // Act
+            var result = sut.PrimeFactorsOf(number);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
