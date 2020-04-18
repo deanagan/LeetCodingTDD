d164ac8ee6f107c205c3ee0cb9024b888ee7df01
Dean Agan <agandfr@gmail.com>
2020-04-18 19:53:35 +1000

    Implement prime factorisation to pass 1 test

--- PrimeFactors/PrimeFactorsLib/PrimeFactors.cs
+++ PrimeFactors/PrimeFactorsLib/PrimeFactors.cs
@@ -7,7 +7,41 @@ namespace PrimeFactorsLib
     {
         public List<int> PrimeFactorsOf(int number)
         {
-            return new List<int>();
+            var factors = new List<int>();
+
+            // One is always a factor
+            factors.Add(1);
+
+            // Two is a factor if divisible by 2
+            if (number % 2 == 0)
+            {
+                factors.Add(2);
+                number /= 2;
+            }
+
+            // Find other factors
+            Func<int,int> IntSqRoot = (n) => Convert.ToInt32(Math.Sqrt(n));
+            var factor = 3;
+            var stop_at = IntSqRoot(number);
+
+            while (factor <= stop_at)
+            {
+                while (number % factor == 0)
+                {
+                    factors.Add(factor);
+                    number = number / factor;
+                    stop_at = IntSqRoot(number);
+                }
+
+                factor += 2;
+            }
+
+            if (number > 1)
+            {
+                factors.Add(number);
+            }
+
+            return factors;
         }
     }
 }
