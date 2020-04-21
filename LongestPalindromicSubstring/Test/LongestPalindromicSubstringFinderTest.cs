22604ed8d8e06adfc5566d7b124a9c1c50337ed3
Dean Agan <agandfr@gmail.com>
2020-04-21 17:44:36 +1000

    Create failing tests

--- LongestPalindromicSubstring/Test/LongestPalindromicSubstringFinderTest.cs
+++ LongestPalindromicSubstring/Test/LongestPalindromicSubstringFinderTest.cs
@@ -1,14 +1,22 @@
 using System;
 using Xunit;
+using FluentAssertions;
 
-namespace Test
+namespace Lib.Test
 {
-    public class UnitTest1
+    public class LongestPalindromicSubstringFinderTest
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(new object[]{"babad", new string[] {"bab", "aba"}})]
+        [InlineData(new object[]{"cbbd", new string[] {"bb"}})]
+        public void PalindromSubstringCorrect_WhenReceivingString(string input, string[] possiblePalindromes)
         {
-
+            // Arrange
+            var sut = new LongestPalindromicSubstringFinder();
+            // Act
+            var result = sut.LongestPalindrome(input);
+            // Assert
+            result.Should().BeOneOf(possiblePalindromes);
         }
     }
 }
