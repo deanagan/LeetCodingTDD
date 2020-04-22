008bd61bc1a49410a16e43b7474f732347fa6528
Dean Agan <agandfr@gmail.com>
2020-04-22 18:08:43 +1000

    Add naive solution

--- LongestPalindromicSubstring/Lib/LongestPalindromicSubstringFinder.cs
+++ LongestPalindromicSubstring/Lib/LongestPalindromicSubstringFinder.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Lib
 {
     public class LongestPalindromicSubstringFinder
     {
         public string LongestPalindrome(string s) {
-            return string.Empty;
+            if (s.Length == 1)
+            {
+                return s;
+            }
+            var length = s.Length;
+            var longestPalindrome = string.Empty;
+            while (length > 1)
+            {
+                var i = 0;
+                while (i + length <= s.Length)
+                {
+                    var sub = s.Substring(i, length);
+                    if (sub == string.Join(string.Empty, sub.Reverse()))
+                    {
+                        if (length > longestPalindrome.Length)
+                        {
+                            longestPalindrome = sub;
+                        }
+                    }
+                    i++;
+                }
+
+                length--;
+            }
+            return longestPalindrome;
         }
     }
 }
