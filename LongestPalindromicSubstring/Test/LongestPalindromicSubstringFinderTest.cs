4905afecde2cbf6982dcdf0ed0e9a9a2914f9c5b
Dean Agan <agandfr@gmail.com>
2020-04-22 18:08:56 +1000

    Add more test

--- LongestPalindromicSubstring/Test/LongestPalindromicSubstringFinderTest.cs
+++ LongestPalindromicSubstring/Test/LongestPalindromicSubstringFinderTest.cs
@@ -11,6 +11,8 @@ namespace Lib.Test
         [InlineData(new object[]{"babad", new string[] {"bab", "aba"}})]
         [InlineData(new object[]{"cbbd", new string[] {"bb"}})]
         [InlineData(new object[]{"a", new string[] {"a"}})]
+        [InlineData(new object[]{"ac", new string[] {"a", "c"}})]
+        [InlineData(new object[]{"", new string[] {""}})]
         public void PalindromSubstringCorrect_WhenReceivingString(string input, string[] possiblePalindromes)
         {
             // Arrange
