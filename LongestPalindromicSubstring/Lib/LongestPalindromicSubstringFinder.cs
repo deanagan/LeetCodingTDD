30aa27129d9fd24e0c5f4489e44a891bf515b07f
Dean Agan <agandfr@gmail.com>
2020-04-24 20:09:01 +1000

    Add a more tedious but faster solution

--- LongestPalindromicSubstring/Lib/LongestPalindromicSubstringFinder.cs
+++ LongestPalindromicSubstring/Lib/LongestPalindromicSubstringFinder.cs
@@ -1,34 +1,71 @@
-﻿using System.Collections.Generic;
+﻿using System.Text;
 using System.Linq;
 
 namespace Lib
 {
     public class LongestPalindromicSubstringFinder
     {
+        private string ScanNLengthPalindrome(int index, string s, bool isOddLengthPalindrome)
+        {
+            var isPalindrome = false;
+            var length = isOddLengthPalindrome ? 1 : 0;
+            var offset = isOddLengthPalindrome ? 1 : 0;
+            var rem = isOddLengthPalindrome ? 0 : 1;
+            var sb = new StringBuilder();
+
+            if (isOddLengthPalindrome == true)
+            {
+                sb.Append(s[index]);
+            }
+
+            do {
+                isPalindrome = s[index + offset] == s[index - (rem + offset)];
+                if (isPalindrome)
+                {
+                    sb.Append(s[index + offset]);
+                    sb.Insert(0, s[index + offset]);
+                }
+                length += 2;
+                offset++;
+
+            } while(isPalindrome && (index + offset < s.Length) && ((index - (rem + offset)) >= 0));
+
+            return sb.ToString();
+        }
+
         public string LongestPalindrome(string s) {
             if (s.Length < 2)
             {
                 return s;
             }
-            var length = s.Length;
-            var longestPalindrome = string.Join(string.Empty, s.First());
-            while (length > 1)
+            if (s.Length == 2)
             {
-                var i = 0;
-                while (i + length <= s.Length)
+                if (s[0] == s[1])
                 {
-                    var sub = s.Substring(i, length);
-                    if (sub == string.Join(string.Empty, sub.Reverse()))
-                    {
-                        if (length > longestPalindrome.Length)
-                        {
-                            longestPalindrome = sub;
-                        }
-                    }
-                    i++;
+                    return s;
+                }
+                return s[0].ToString();
+            }
+            var length = s.Length;
+            var longestPalindrome = string.Empty;
+            for(var index = 1; index < (length - 1); index++)
+            {
+                var p = ScanNLengthPalindrome(index, s, true);
+                if (p.Length > longestPalindrome.Length)
+                {
+                    longestPalindrome = p;
                 }
 
-                length--;
+                p = ScanNLengthPalindrome(index, s, false);
+                if (p.Length > longestPalindrome.Length)
+                {
+                    longestPalindrome = p;
+                }
+            }
+
+            if ((s[length - 1] == s[length - 2]) && (longestPalindrome.Length < 2))
+            {
+                longestPalindrome = s[length - 1].ToString() + s[length - 2].ToString();
             }
             return longestPalindrome;
         }
