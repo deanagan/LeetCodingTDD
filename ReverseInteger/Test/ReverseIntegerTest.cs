5cc845dea3c25f879cdedbeee7390e2b050bc73b
Dean Agan <agandfr@gmail.com>
2020-04-25 17:42:59 +1000

    Add test case for reversed number beginning with zero. Also added overflow scenario.

--- ReverseInteger/Test/ReverseIntegerTest.cs
+++ ReverseInteger/Test/ReverseIntegerTest.cs
@@ -10,6 +10,8 @@ namespace Lib.Test
         [InlineData(new object[] {0, 0})]
         [InlineData(new object[] {123, 321})]
         [InlineData(new object[] {-123, -321})]
+        [InlineData(new object[] {120, 21})]
+        [InlineData(new object[] {1534236469, 0})]
         public void ReverseInteger(int input, int expected)
         {
             // Arrange
