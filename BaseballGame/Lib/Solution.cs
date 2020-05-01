f5adedb4f713108aa80a023f4137970bf215973b
Dean Agan <agandfr@gmail.com>
2020-05-01 16:52:36 +1000

    Add solution to pass first test

--- BaseballGame/Lib/Solution.cs
+++ BaseballGame/Lib/Solution.cs
@@ -1,11 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Lib
 {
     public class Solution
     {
         public int CalPoints(string[] ops) {
-            return 0;
+            var infoStack = new Stack<int>();
+            var total = 0;
+            foreach(var info in ops) {
+                if (info == "C") {
+                    if (infoStack.Count > 0)
+                    {
+                        var m = infoStack.Pop();
+                        total -= m;
+                    }
+                } else if (info == "D") {
+                    var dresult = infoStack.Peek() * 2;
+                    infoStack.Push(dresult);
+                    total += dresult;
+                } else if (info == "+") {
+                    var last2total = infoStack.Skip(infoStack.Count - 2).Sum();
+                    total += last2total;
+                } else {
+                    infoStack.Push(Convert.ToInt32(info));
+                    total += infoStack.Peek();
+                }
+
+            }
+            return total;
         }
     }
 }
